3de90ed780d6e3de00b6934063bc033154299ab2
labram13 <labradml@gmail.com>
2026-04-08 18:05:42 -0700

    connected to server on auth request

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -8,17 +8,35 @@ type FormValues = {
     username: string,
     email: string, 
     password: string,
-    confirmPassword: string,
+    confirmPassword?: string,
 }
-export default function Register() {
+
+
+export default  function Register() {
     const form = useForm<FormValues>()
     const { register, control, handleSubmit, formState, setError, watch} = form
     const { errors } = formState;
 
 
-    const onSubmit = (data: FormValues) => {
+    const onSubmit = async (data: FormValues) => {
+        const {confirmPassword, ...newUser}:FormValues = data
 
-     console.log(data)
+        console.log(newUser)
+
+
+        
+
+        const response = await fetch('http://localhost:3001/api/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                newUser: newUser
+            })
+
+        })
+     
 
     }
 
@@ -32,8 +50,8 @@ export default function Register() {
                 <h2>Register</h2>
             </div>
             <div className="input">
-                <input type='text' id='username' placeholder='Username' {...register("username", 
-                    {required: {
+                <input type='text' id='username' placeholder='Username' {...register("username", {
+                    required: {
                         value: true,
                         message: "Username is required"}}
                 )}/>
--- server/src/routes/controller/auth.ts
+++ server/src/routes/controller/auth.ts
@@ -1,11 +1,23 @@
 import express from 'express'
+import pool from '../../config/db'
 
 const router = express.Router();
 
 
+type NewUser = {
+    username:string,
+    email:string,
+    password:string
+}
+
+type RegisterResponse = {
+    usernameStatus: string,
+    emailStatus: string
+}
 router.post('/', (req, res) => {
 
-
+    const newUser: NewUser = req.body
+    console.log(newUser)
     res.status(200).json({status: 'success'})
 })
 
