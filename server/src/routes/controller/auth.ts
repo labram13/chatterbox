442f37dbdf84b0c04133e8e0a6ff1283e96d134b
labram13 <labradml@gmail.com>
2026-04-16 18:51:20 -0700

    added check to see if users exists

--- server/src/routes/controller/auth.ts
+++ server/src/routes/controller/auth.ts
@@ -1,23 +1,37 @@
 import express from 'express'
-import pool from '../../config/db'
+import pool from '../../config/db.js'
 
 const router = express.Router();
 
 
-type NewUser = {
+type User = {
     username:string,
     email:string,
     password:string
 }
 
 type RegisterResponse = {
-    usernameStatus: string,
-    emailStatus: string
+    message: string
 }
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
-    const newUser: NewUser = req.body
-    console.log(newUser)
+    const newUser: User = req.body
+    // console.log(newUser)
+
+    const userExists = await pool.query(
+        'SELECT * FROM users where username = $1 OR email = $2',
+        [newUser.username, newUser.email]
+    )
+
+    if (userExists.rows.length !== 0) {
+        return res.status(400).json({message: "user exists"})
+    }
+
+    const addUser = await pool.query(
+        'INSERT INTO users ('
+    )
+
+    
     res.status(200).json({status: 'success'})
 })
 
