507e9ab17c7a44f3b009a130352c6c7af8610fe6
labram13 <labradml@gmail.com>
2026-07-05 17:04:21 -0700

    added notes for socket.io usage

--- server/src/config/socket.ts
+++ server/src/config/socket.ts
@@ -1,6 +1,26 @@
 import {Server as httpServer} from 'http'
 import {Server as socketServer, Socket} from 'socket.io'
 
+/*
+Notes for Socket.io usage
+
+-initialize connection to server from client side using socket= = io(localhost)
+-for specific room join, emit a custom event for 'join room' with a passed arguement (room id)
+    - once done, server side listens for the event 'join room', then uses socket.join(room) from 'arg' to establish
+    connection to the room
+-in order to emit messages to the specific room and to everyone else but the sender,
+we use socket.to
+    -client emits message with room id as arg as socket.emit('new message', roomid)
+        - can add other objects to payload so can be {roomid: roomid, message: message}
+    -server listens for new message event socket.on('new message')
+    -once server hears the event, we grab the room id from arg and emit it to other users
+        - example: socket.to(arg.roomid).emit('new message', arg.message)
+- finally, client has a useEffect that listens to the event 'new-message'
+    - user will only get events inside 'new message' if only part of the room emitted to (socket.to(arg.roomid))
+
+
+*/
+
 export default function socketConnection(httpServer: httpServer): socketServer {
     const io = new socketServer(httpServer, {
         cors: {
